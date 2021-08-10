0873cc18c42e7943bf4480cddf31a734186f1d9c
Marlon Santos <xsantos.marlonx@gmail.com>
2021-08-09 23:14:46 -0300

    Created the class HourContract

---
+++ Exercise/Entities/HourContract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValueperHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valueperHour, int hours)
+        {
+            Date = date;
+            ValueperHour = valueperHour;
+            Hours = hours;
+        }
+        public double TotalValue()
+        {
+            return ValueperHour * Hours;
+        }
+    }
+}
